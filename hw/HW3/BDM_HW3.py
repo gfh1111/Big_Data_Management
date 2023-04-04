e36b1237e69f84af257028800b0b0e07c7e1bc02
gfh1111 <94469476+gfh1111@users.noreply.github.com>
2023-04-04 14:50:15 -0400

    Update BDM_HW3.py

--- hw/HW3/BDM_HW3.py
+++ hw/HW3/BDM_HW3.py
@@ -1,33 +1,49 @@
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
 import sys
-import csv
 
+def step1(partId, rows):
+    if partId==0:
+       next(rows)
+    import csv
+    reader = csv.reader(rows)
+    for row in reader:
+        (year,product,company) = (row[0][0:4],row[1].lower(),row[7].lower())
+        yield (year,product,company)
 
-if __name__=='__main__':
+if __name__=="__main__":
+    #sc = SparkContext()
+    sc = pyspark.SparkContext.getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
     
-    sc = SparkContext()
+    complains = sc.textFile(sys.argv[1], use_unicode=True).cache()  
+    A = complains.mapPartitionsWithIndex(step1).saveAsTextFile(sys.argv[2])
 
-    input = sys.argv[1]
-    output_folder = sys.argv[2]
+#     #total number of complains
+#     B = A.map(lambda x: ((x[1],x[0]),1))\
+#          .reduceByKey(lambda x,y: x+y)
 
-    cpl = sc.textFile(input, use_unicode=True).cache()
+#     #total number of companies
+#     C = A.map(lambda x: ((x[1],x[0],x[2]),1))\
+#          .reduceByKey(lambda x,y: x+y)\
+#          .map(lambda x: ((x[0][0],x[0][1]),1))\
+#          .reduceByKey(lambda x,y: x+y)
 
-    def mapper(partitionId, records):
-        if partitionId == 0:
-            next(records)
-            reader = csv.reader(records,delimiter=',',quotechar='"')
-            for r in reader:
-                if len(r)>7 and type(r[0])==str and len(r[0])==10 and r[0][0]==('2'or'1'):
-                    yield r[0][0:4], \
-                        r[1].lower().replace('"', "'") if ',' in r[1] else r[1].lower(), \
-                        r[7].lower()
-                    
-
-    cpl.mapPartitionsWithIndex(mapper) \
-        .map(lambda x: ((x[1],x[0],x[2]),1)) \
-        .reduceByKey(lambda x,y: x+y) \
-        .map(lambda x: ((x[0][0],x[0][1]),(x[1],1,x[1]))) \
-        .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], max(x[2],y[2]))) \
-        .mapValues(lambda x: (x[0],x[1], round(x[2]/x[0]*100))) \
-        .sortByKey() \
-        .saveAsTextFile(output_folder)
+#     #highest number of complains received by a company   
+#     D = A.map(lambda x: ((x[1],x[0],x[2]),1))\
+#          .reduceByKey(lambda x,y: x+y)\
+#          .map(lambda x: ((x[0][0],x[0][1]),x[1]))\
+#          .reduceByKey(lambda x,y: max(x,y))
+#          .saveAsTextFile(sys.argv[2])
+    
+#     #expected output
+#     E = B.join(C).join(D)\
+#          .map(lambda x: (x[0],(x[1][0][0],x[1][0][1],x[1][1])))\
+#          .map(lambda x: ((x[0][0],x[0][1]),(str(x[1][0]),str(x[1][1]),str(round(x[1][2]/x[1][0]*100)))))\
+#          .sortByKey(ascending=True, numPartitions=None)\
+#          .map(lambda x: x[0]+x[1])
+ 
+#     E.map(lambda x: ','.join(str(item) for item in x))\
+#      .saveAsTextFile(sys.argv[2])
